e7c8313ce81abbbe53b5c9bb44c1b84b8d1bb2b1
giuspy <s241962@polito.studenti.it>
2017-07-05 13:09:30 +0200

    First commit
    First commit

---
+++ app/app.js
@@ -0,0 +1,48 @@
+'use strict';
+
+// Initialize the Firebase SDK
+var config = {
+    apiKey: "AIzaSyCqVP5fLNwraKgKpDA1GE-_3EFmL_Sf9tk",
+    authDomain: "frizzy-db697.firebaseapp.com",
+    databaseURL: "https://frizzy-db697.firebaseio.com",
+    projectId: "frizzy-db697",
+    storageBucket: "frizzy-db697.appspot.com",
+    messagingSenderId: "437509073990"
+};
+firebase.initializeApp(config);
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', [
+    "firebase",
+    'ngRoute',
+    'myApp.loginView',
+    'myApp.authentication',
+    'myApp.userRegistrationView'
+])
+    .config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+        $locationProvider.hashPrefix('!');
+        $routeProvider.otherwise({redirectTo: '/eventoView'});
+    }])
+    .run(["$rootScope", "$location", function($rootScope, $location) {
+        $rootScope.$on("$routeChangeError", function(event, next, previous, error) {
+            // We can catch the error thrown when the $requireSignIn promise is rejected
+            // and redirect the user back to the home page
+            if (error === "AUTH_REQUIRED") {
+                $location.path("/loginView");
+            }
+        });
+    }])
+    .controller('MainCtrl', ['$scope', '$rootScope', '$firebaseAuth', function($scope, $rootScope, $firebaseAuth) {
+        //this controller only declares a function to get information about the user status (logged in / out)
+        //it is used to show menu buttons only when the user is logged
+        //set the variable that is used in the main template to show the active button
+        $rootScope.dati = {};
+        $rootScope.dati.currentView = 'home';
+        $scope.isLogged = function()
+        {
+            if ($firebaseAuth().$getAuth())
+                return true;
+            else
+                return false;
+        }
+    }]);
---
+++ app/eventoView/eventoView.js
@@ -0,0 +1,41 @@
+'use strict';
+
+angular.module('myApp.eventoView', ['ngRoute','myApp.evento'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/eventoView', {
+    templateUrl: 'eventoView/eventoView.html',
+    controller: 'View1Ctrl',
+      resolve: {
+          // controller will not be loaded until $requireSignIn resolves
+          // Auth refers to our $firebaseAuth wrapper in the factory below
+          "currentAuth": ["Auth", function(Auth) {
+              // $requireSignIn returns a promise so the resolve waits for it to complete
+              // If the promise is rejected, it will throw a $routeChangeError (see above)
+              return Auth.$requireSignIn();
+          }]
+
+      }
+  })
+}])
+
+.controller('View1Ctrl', ['$scope','$rootScope', 'Evento', function($scope,$rootScope, Evento) {
+    //initialize variables
+    $scope.dati={};
+    $scope.dati.vm = this;
+    $scope.dati.vm.positions = [];
+    //set the variable that is used in the main template to show the active button
+    $rootScope.dati.currentView = "home";
+    //get the list of available pizzas
+    $scope.dati.eventi = Evento.getData();
+    //$scope.googleMapsUrl="https://maps.googleapis.com/maps/api/js?key=AIzaSyD6qAQOEvZs2XlUUu3ziu-nrDX-WWZXap4";
+    //only when all data will be loaded, the map will be created
+ /*   $scope.dati.eventi.$loaded().then(function () {
+        for (var i = 0; i < $scope.dati.eventi.length; i++) {
+            var lat = 45.071087 + (Math.random() / 100);
+            var lng = 7.686567 + (Math.random() / 100);
+            $scope.dati.vm.positions.push({lat: lat, lng: lng});
+        }
+    }
+    );*/
+}]);
